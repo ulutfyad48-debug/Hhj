b59ee1f463c2d56b2bb7d595b073495f6f8071ed
YADULLAH <ulutfyad48@gmail.com>
2026-02-13 14:01:49 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,25 @@
-const CACHE_NAME = 'myshop-v1';
+// Service Worker for PWA (Offline Support & Caching)
+const CACHE_NAME = 'site-v2';
 const assets = [
-  './',
-  './index.html',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'
+  '/',
+  '/index.html',
+  '/data.js',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'
 ];
 
-// انسٹالیشن کے وقت فائلیں سیو کرنا
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      cache.addAll(assets);
+      return cache.addAll(assets);
     })
   );
 });
 
-// نیٹ ورک نہ ہونے پر کیش سے دکھانا
 self.addEventListener('fetch', evt => {
   evt.respondWith(
-    caches.match(evt.request).then(rec => {
-      return rec || fetch(evt.request);
+    caches.match(evt.request).then(res => {
+      return res || fetch(evt.request);
     })
   );
 });
